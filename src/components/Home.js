308120949ef68f5536c58b24adb858cf558ea79f
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-02-20 02:15:11 -0800

    Home: Arranging components for the portfolio

---
+++ src/components/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Hero from './Hero'
+import About from './About'
+import Services from './Services'
+import Contact from './Contact'
+import Projects from './Projects'
+import Work from './Work'
+
+
+const Home = () => {
+  return (
+    <div className="">
+        <Hero/>
+        <About/>
+        <Services/>
+        <Work/>
+        <Projects/>
+        <Contact/>
+    </div>
+  )
+}
+
+export default Home
