0201b36427f9e148d6fd4fcc0cf2ddad97143948
Naggayi Daphne Pearl <daphnepearl101@gmail.com>
2023-02-27 03:57:38 -0800

    links

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,7 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import myResume from "../assets/resume/my-cv.pdf";
 import { FiMenu } from "react-icons/fi";
-import {
-  FaLinkedinIn,
-  FaGithub,
-  FaTwitter,
-  FaAngleRight,
-} from "react-icons/fa";
 
 function Navbar() {
   const navlinks = [
@@ -14,7 +9,6 @@ function Navbar() {
     { id: 2, text: "Services", href: "/services" },
     { id: 3, text: "Projects", href: "/projects" },
     { id: 4, text: "Contacts", href: "/contact" },
-    
   ];
 
   const [isOpen, setIsOpen] = useState(false);
@@ -25,44 +19,43 @@ function Navbar() {
 
   return (
     <div className="relative container mx-auto">
-      <div className="w-full z-40 top-0 py-4 sm:py-8  absolute"></div>
+      <div className="w-full z-40 top-0 py-4 sm:py-8 absolute"></div>
       {/* Flex Container */}
       <div className="flex items-center justify-between">
         {/* Logo */}
         <div className="pt-2">
-          <a href="/">
-            <h4 className="w-48 text-yellow lg:text-5xl pr-1 my-2  font-['Open_Sans'] text-3xl mx-4 font-extrabold">
+          <Link to="/">
+            <h4 className="w-48 text-yellow lg:text-5xl pr-1 my-2 font-open-sans text-3xl mx-4 font-extrabold">
               DAPHNE
             </h4>
-          </a>
+          </Link>
         </div>
-        {/* Menu items fpr large screens */}
+        {/* Menu items for large screens */}
         <div className="hidden space-x-6 md:flex">
           <ul className="flex items-center text-xl ml-12">
             {navlinks.map((item) => (
-              <a href={item.href}>
-                <li key={item.id}>
+              <Link to={item.href} key={item.id}>
+                <li>
                   <span className="cursor-pointer pt-0.5 font-header text-2xl font-semibold uppercase text-yellow pl-6 ">
                     {item.text}
                   </span>
                   <span className="block h-0.5 w-full bg-transparent hover:bg-primary"></span>
                 </li>
-              </a>
+              </Link>
             ))}
           </ul>
         </div>
 
-        {/* Menu items for small screen */}
-
+        {/* Menu items for small screens */}
         <div className="lg:hidden">
           <button
             id="menu-btn"
             onClick={toggleMenu}
-            className="flex items-center px-4 mx-4 py-2  text-yellow  text-4xl"
+            className="flex items-center px-4 mx-4 py-2 text-yellow text-4xl"
           >
             <FiMenu />
 
-            {/* Mobile menu  */}
+            {/* Mobile menu */}
             <div
               className={`${
                 isOpen ? "" : "hidden"
@@ -70,9 +63,13 @@ function Navbar() {
             >
               <ul className="text-sm lg:flex-grow">
                 {navlinks.map((item) => (
-                  <a key={item.id} href={item.href}>
-                    <li className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4">{item.text}</li>
-                  </a>
+                  <Link
+                    to={item.href}
+                    key={item.id}
+                    className="block mt-4 lg:inline-block lg:mt-0 text-blue-200 hover:text-white mr-4"
+                  >
+                    {item.text}
+                  </Link>
                 ))}
               </ul>
             </div>
@@ -80,8 +77,6 @@ function Navbar() {
         </div>
       </div>
     </div>
-
-    // Hero section
   );
 }
 
