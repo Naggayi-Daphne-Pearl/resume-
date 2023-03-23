a545ce8d7c4e38f4feefdc7d95f8c9d0446bf27b
Naggayi Daphne Pearl <98743233+Naggayi-Daphne-Pearl@users.noreply.github.com>
2023-03-23 17:30:18 +0300

    Update Hero.js

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import Navbar from "./Navbar";
-import backgroundImage from "../assets/img/background.jpg";
+// import backgroundImage from "../assets/img/background.jpg";
 import {
   FaLinkedinIn,
   FaGithub,
